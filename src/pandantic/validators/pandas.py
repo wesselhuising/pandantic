7388c957c14c9f50299f42252000610afdc93dc2
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2024-10-02 18:39:57 -0400

    Switch to copy free version!

--- src/pandantic/validators/pandas.py
+++ src/pandantic/validators/pandas.py
@@ -1,6 +1,7 @@
 import logging
 import math
 import os
+from collections.abc import Hashable
 from typing import Any, Optional
 
 import pandas as pd
@@ -44,10 +45,7 @@ class PandasValidator(BaseValidator):
         errors_index = []
         logging.debug("Amount of available cores: %s", os.cpu_count())
 
-        dataframe = dataframe.copy()
-        dataframe["_index"] = dataframe.index
-
-        if n_jobs != 1:
+        if n_jobs < 1:
             if n_jobs < 0:
                 n_jobs = os.cpu_count()  # type: ignore
 
@@ -55,24 +53,29 @@ class PandasValidator(BaseValidator):
             chunk_size = math.floor(len(dataframe) / n_jobs)
             num_chunks = len(dataframe) // chunk_size + 1
 
+            # handle user input for queue
             if queue is None:
                 queue = Queue()
+            elif "queue" not in str(type(queue)).lower():
+                logging.warning(f"Expecting queue object for arg:queue, not {type(queue)}!")
+            else:
+                assert hasattr(queue, "get"), "Queue object must have a put method."
+                assert hasattr(queue, "put"), "Queue object must have a put method."
 
-            q = queue
-
+            # send chunks to be processed
             for i in range(num_chunks):
-                chunks.append(dataframe.iloc[i * chunk_size : (i + 1) * chunk_size])
-
-            for i in range(num_chunks):
-                p = Process(
-                    target=self._validate_chunk, args=(chunks[i], q), daemon=True
+                chunks.append(
+                    dataframe.iloc[i * chunk_size : (i + 1) * chunk_size].to_dict("index")
                 )
+
+            for i in range(num_chunks):
+                p = Process(target=self._validate_chunk, args=(chunks[i], queue), daemon=True)
                 p.start()
 
             num_stops = 0
             for i in range(num_chunks):
                 while True:
-                    index = q.get()
+                    index = queue.get()
                     if index is None:
                         num_stops += 1
                         break
@@ -82,18 +85,18 @@ class PandasValidator(BaseValidator):
                 if num_stops == num_chunks:
                     break
         else:
-            for row in dataframe.to_dict("records"):
+            for index, row_dict in dataframe.to_dict("index").items():
                 try:
                     self.schema.model_validate(
-                        obj=row,
+                        obj=row_dict,
                         context=context,
                     )
                 except PydanticValidationError as exc:  # pylint: disable=broad-exception-caught
                     if verbose:
                         print(exc)
-                        logging.info("Validation error found at index %s\n%s", row["_index"], exc)
+                        logging.info("Validation error found at index %s\n%s", index, exc)
 
-                    errors_index.append(row["_index"])
+                    errors_index.append(index)
 
         logging.debug("# invalid rows: %s", len(errors_index))
 
@@ -102,11 +105,11 @@ class PandasValidator(BaseValidator):
         if len(errors_index) > 0 and errors == "filter":
             return dataframe[~dataframe.index.isin(list(errors_index))].drop(columns=["_index"])
 
-        return dataframe.drop(columns=["_index"])
+        return dataframe
 
     def _validate_chunk(
         self,
-        chunk: pd.DataFrame,
+        chunk: dict[Hashable, Any],
         q: Queue,
         context: Optional[
             dict[str, Any]
@@ -123,17 +126,17 @@ class PandasValidator(BaseValidator):
         """
         logging.debug("Process started.")
 
-        for row in chunk.to_dict("records"):
+        for index, row_dict in chunk.items():
             try:
                 self.schema.model_validate(
-                    obj=row,
+                    obj=row_dict,
                     context=context,
                 )
             except PydanticValidationError as exc:  # pylint: disable=broad-exception-caught
                 if verbose:
-                    logging.info("Validation error found at index %s\n%s", row["_index"], exc)
+                    logging.info("Validation error found at index %s\n%s", index, exc)
 
-                q.put(row["_index"])
+                q.put(index)
 
         logging.debug("Process ended.")
         q.put(None)
