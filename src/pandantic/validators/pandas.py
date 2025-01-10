4fe5c42a8c167bf799925ff397d13143a48ccd57
Wessel Huising <wesselhuising@hotmail.nl>
2025-01-10 16:17:41 +0100

    Implement strict parameter for strict mode (#33)
    * add strict mode
    
    * add test for strict mode
    
    * black and mypy fixes
    
    * 📝 adding to docstring
    
    * ✏️ refining logic for strict mode
    
    * 📝 adding note for future dev
    
    * ✏️ failing on first error, not after all errors
    
    ---------
    
    Co-authored-by: Wessel Huising <wessel@MacBook-Pro-van-Wessel.local>
    Co-authored-by: Xavier Nogueira <xavier.rojas.nogueira@gmail.com>

--- src/pandantic/validators/pandas.py
+++ src/pandantic/validators/pandas.py
@@ -23,6 +23,7 @@ class PandasValidator(BaseValidator):
         self,
         dataframe: pd.DataFrame,
         errors: Literal["skip", "raise", "log"] = "raise",
+        strict: bool = False,
         context: Optional[
             dict[str, Any]
         ] = None,  # pylint: disable=consider-alternative-union-syntax,useless-suppression
@@ -35,6 +36,7 @@ class PandasValidator(BaseValidator):
             dataframe (pd.DataFrame): The DataFrame to validate.
             errors (Literal["skip", "raise", "log"], optional): How to handle validation errors. Defaults to "raise".
                 NOTE: "skip" and "log" effectively filter the dataframe, excluding invalid rows.
+            strict (bool, default=False): whether to fail validation if extra fields/columns are present.
             context (Optional[dict[str, Any]], optional): The context to use for validation. Defaults to None.
             n_jobs (int, optional): The number of processes to use for validation. Defaults to 1.
             queue (Optional[Queue], optional): A custom Queue object for multiprocessing. Defaults to None.
@@ -42,6 +44,18 @@ class PandasValidator(BaseValidator):
         Returns:
             pd.DataFrame: The original DataFrame if errors="raise" or "log", or a filtered DataFrame with valid rows if errors="skip".
         """
+        # check for extra columns and handle strict mode
+        # NOTE: this will need to be abstracted to handle different types of schema objects
+        if strict:
+            extras = {
+                col for col in dataframe.columns if col not in self.schema.model_fields.keys()
+            }
+            if extras:
+                raise ValueError(
+                    f"Strict mode is enabled but the following extra columns were found in the schema: {extras}."
+                )
+            del extras
+
         if errors not in ["skip", "raise", "log"]:
             raise ValueError("errors must be one of 'skip', 'raise', or 'log'")
 
@@ -101,13 +115,12 @@ class PandasValidator(BaseValidator):
                 except ValidationError as exc:  # pylint: disable=broad-exception-caught
                     if errors == "log":
                         logging.info("Validation error found at index %s\n%s", index, exc)
-
+                    if errors == "raise":
+                        raise exc
                     errors_index.append(index)
 
         logging.debug("# invalid rows: %s", len(errors_index))
 
-        if len(errors_index) > 0 and errors == "raise":
-            raise ValueError(f"{len(errors_index)} validation errors found in dataframe.")
         if len(errors_index) > 0 and errors in ["skip", "log"]:
             return dataframe[~dataframe.index.isin(list(errors_index))]
         return dataframe
@@ -141,7 +154,8 @@ class PandasValidator(BaseValidator):
             except ValidationError as exc:  # pylint: disable=broad-exception-caught
                 if errors == "log":
                     logging.info("Validation error found at index %s\n%s", index, exc)
-
+                if errors == "raise":
+                    raise exc
                 queue.put(index)
 
         logging.debug("Process ended.")
@@ -159,9 +173,12 @@ class PandasValidator(BaseValidator):
         """Iterate over a DataFrame and yield validated schema models."""
         for i, row in dataframe.iterrows():
             try:
-                yield i, self.schema.model_validate(
-                    obj=row.to_dict(),
-                    context=context,
+                yield (
+                    i,
+                    self.schema.model_validate(
+                        obj=row.to_dict(),
+                        context=context,
+                    ),
                 )
             except Exception as e:
                 if verbose:
